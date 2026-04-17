b43e7baf19d56ba2bf37c01518e372d544795244
Taichi Shimizu <tai.smz55@gmail.com>
2026-04-17 11:10:38 +0900

    20260417

--- tests/research/test_weakly_stable_exists.cpp
+++ tests/research/test_weakly_stable_exists.cpp
@@ -9,23 +9,23 @@ TEST(Research, WeaklyStableExists_CounterCandidate)
 {
     const int n_agents = 4;
     const int n_firms  = 2;
-    const vector<int> capacities = {1, 3};
+    const vector<int> capacities = {2, 2};
 
     vector<vector<int>> agent_prefs = {
+        {1, 2},
         {2, 1},
         {2, 1},
-        {2, 1},
-        {2, 1}
+        {1, 2}
     };
     vector<vector<int>> firm_prefs = {
-        {3, 4, 2, 1},
-        {3, 1, 4, 2}
+        {1, 4, 2, 3},
+        {2, 4, 3, 1}
     };
     vector<vector<int>> agent_col_prefs = {
-        {0,                   0, 0,                   0},
-        {BINARY_PREF_NEG_INF, 0, BINARY_PREF_NEG_INF, BINARY_PREF_NEG_INF},
-        {0,                   0, 0,                   BINARY_PREF_NEG_INF},
-        {0,                   0, 0,                   0}
+        {0,                   0, 0, 0},
+        {0,                   0, 0, 0},
+        {BINARY_PREF_NEG_INF, 0, 0, 0},
+        {BINARY_PREF_NEG_INF, 0, 0, 0}
     };
 
     MatchingSystem ms(n_agents, n_firms);
@@ -38,13 +38,11 @@ TEST(Research, WeaklyStableExists_CounterCandidate)
     bool exists = false;
     int idx = 0;
     for (auto &m : all_matchings) {
-
         if (m.is_weakly_stable(agent_prefs, firm_prefs, agent_col_prefs, capacities)) {
             cout << "This matching is weakly stable." << endl;
             m.print();
             exists = true;
         } 
-        cout << endl;
     }
 
     if (!exists) {
